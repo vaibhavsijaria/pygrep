ce3787496afb9d9e58c2945030cdea796fba60c6
Vaibhav Sijaria <139199971+vaibhavsijaria@users.noreply.github.com>
2024-02-23 19:19:24 +0530

    Add count option to pygrep command

--- pygrep/__main__.py
+++ pygrep/__main__.py
@@ -9,13 +9,18 @@ def main():
     args = parse_args()
     searchStr = args.searchStr
     path = args.path
-
+    c = 0
     for file_path in file_handler(path,args.recursive):
         for line in read_file(file_path):
             file_path = file_path if args.recursive else None
             matches = str_search(line,searchStr,args.ignore_case)
-            printOutput(line,matches,file_path)
+            if args.count:
+                c += 1 if len(matches) else 0
+            else:
+                printOutput(line,matches,file_path)
 
+    if args.count:
+        print(c)
 
 if __name__ == "__main__":
     main()
--- pygrep/utils/argparser.py
+++ pygrep/utils/argparser.py
@@ -6,5 +6,6 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("path")
     parser.add_argument("-r","--recursive",action="store_true")
     parser.add_argument("-i","--ignore-case",action="store_true")
+    parser.add_argument("-c","--count",action="store_true")
 
     return parser.parse_args()
